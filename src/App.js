8b4904ed6b1db1ba915543823dff4fddc93b7982
Damon <voldev@hotmail.com>
2020-01-06 18:33:46 -0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path='/' component={Homepage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
-}
+};
 
 export default App;
