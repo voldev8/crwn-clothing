d8cae02cc8964bf977f8bcd3f48fe9fb7f95b7d3
Damon <voldev@hotmail.com>
2020-01-06 21:34:10 -0500

    updating our app with a shop page, collection preview component and collection item componenet

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
-const Homepage = () => (
+const HomePage = () => (
   <div className="homepage">
     <Directory />
   </div>
 );
 
-export default Homepage;
+export default HomePage;
