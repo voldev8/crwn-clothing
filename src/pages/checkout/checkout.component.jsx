7267077bf8a9e17bddf53b55a4b95a3708243a93
Damon <voldev@hotmail.com>
2020-01-09 19:38:29 -0500

    added functionality to arrows in checkout item component increase-decrease quantity

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -29,7 +29,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     {
       cartItems.map(cartItem => (
-        <CheckoutItem cartItem={cartItem} />
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))
     }
     <div className="total">
